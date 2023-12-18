173c5c4de7b15cfb17347f84f253cc6cd71a3180
Ashutosh Joshi <127975606+coderAj21@users.noreply.github.com>
2023-12-18 19:54:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,13 @@ const PORT=process.env.PORT || 4000;
 
 // middlerware
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://yoga-form-six.vercel.app/"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 
 
 
