49d70a4e8f0c883099ef441897f7e81610365cf9
Ashutosh Joshi <127975606+coderAj21@users.noreply.github.com>
2023-12-18 20:17:51 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,6 @@ const PORT=process.env.PORT || 4000;
 
 // middlerware
 app.use(express.json());
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
 app.use(cors(
     {
         origin:[""],
