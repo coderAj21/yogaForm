5d485d2adb566b0db110000fed52e567c5a2f149
Ashutosh Joshi <127975606+coderAj21@users.noreply.github.com>
2023-12-18 19:37:42 +0530

    Update dbConnect.js

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
-const MONGODB_URL=process.env.MONGODB_URL;
+const MONGODB_URL="mongodb+srv://akartik689:EidjxAkYsnhule6d@cluster1.huwb3qe.mongodb.net/yogaDatabase?retryWrites=true&w=majority"
 
 
 const db=()=>{
